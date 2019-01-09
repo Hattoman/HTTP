d9ebb21fcd911b7dc07f0cb0425ba660c6d95e9d
Alicia Geijer <0002alge@res.ludvika.intra>
2019-01-09 09:01:49 +0100

    avklarat E6

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ console.log(require("module").builtinModules);
 
 const http = require("http");
 
-const httpServer = http.createServer((request, response) => {});
+const httpServer = http.createServer((request, response) => {
+  const route = request.url;
+  console.log(route);
+});
 
 httpServer.listen(1337, () => console.log("Webbserver startad på port 1337"));
