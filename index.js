1f132e41e1ca818acce72240a2c7ddd4492a5a89
Alicia Geijer <0002alge@res.ludvika.intra>
2019-01-09 10:24:53 +0100

    avklarat E9

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ console.log(require("module").builtinModules);
 
 const http = require("http");
 
-const httpServer = http.createServer((request, response) => {
+const httpServer = http.createServer((request /*response*/) => {
   let route = request.url;
   const method = request.method;
   let dynamicArguments = request.url.split("/");
   route = dynamicArguments[1];
   dynamicArguments = dynamicArguments.splice(2);
+  const headers = request.headers;
   console.log(route);
   console.log(method);
   console.log(dynamicArguments);
+  console.log(headers);
 });
 
 httpServer.listen(1338, () => console.log("Servern lyssnar nu på port 1338"));
