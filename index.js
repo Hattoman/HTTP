d860fa375418875e6c27b525161303466d8f1100
Alicia Geijer <0002alge@res.ludvika.intra>
2019-01-09 11:37:01 +0100

    avklarat E10

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("texan cat: meowdy");
 console.log(require("module").builtinModules);
 
 const http = require("http");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 const httpServer = http.createServer((request /*response*/) => {
   let route = request.url;
@@ -11,10 +12,22 @@ const httpServer = http.createServer((request /*response*/) => {
   route = dynamicArguments[1];
   dynamicArguments = dynamicArguments.splice(2);
   const headers = request.headers;
-  console.log(route);
-  console.log(method);
-  console.log(dynamicArguments);
-  console.log(headers);
+  const decoder = new StringDecoder("utf8");
+  let payload = "";
+  request.on("data", data => {
+    payload += decoder.write(data);
+  });
+  request.on("end", () => {
+    payload += decoder.end();
+
+    console.log({
+      route,
+      dynamicArguments,
+      method,
+      headers,
+      payload
+    });
+  });
 });
 
 httpServer.listen(1338, () => console.log("Servern lyssnar nu på port 1338"));
